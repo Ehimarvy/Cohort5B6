f9bb9b54801c4372f5d8b3fd97be30f3676f2a22
Ehimwenma Marvelous <ehimarvy@gmail.com>
2022-10-24 06:59:53 +0100

    second update

---
+++ Class4.py
@@ -0,0 +1,15 @@
+#print('Ehimwenma'+' '+'marvelous'.title())
+fruit="pawpaw" 
+#print(fruit[1:3])
+league='premier'
+#print(league[3:6])
+#REPLACING OF LETTERS
+greeting="Hello World"
+#print('J'+ greeting[1:])
+#a='marvelous'
+#for letters in a :
+    #print(letters)
+prefix='j', 'k', 'l', 'm','o', 'p', 'q'
+suffix='ack'
+for letters in prefix :
+ #   print(letters + suffix)
---
+++ Class5.py
@@ -0,0 +1,88 @@
+#for i in range(10):
+ #   if i%2==0:
+ #       print(i)
+ #   elif i%2 == 1:
+ #       print(i)
+#for numbers in range(-10,0):
+#    print(numbers)
+#username=input("Tell us your Username: ")
+#for letter in username:
+  #  if letter =="a":
+    #    print("true")
+    #else:
+    #    print("false")
+#i=0
+#while i<10:
+#     (i+=1)
+
+#i=0
+#while i in range(3):
+#  name=input("Your name:")
+#  i+=1
+#def print_lyrics():
+#    print("i'm lumberjack,and i am okay.")
+ #   print("i sleep all night and i work all day")
+
+#print_lyrics()
+
+#def greet(name):
+ # print("Hello" " "+ name,"good morning")
+
+#greet("marv")
+
+#def profile(name,age):
+#  print(f"dear {name}, you are {age} years old.")
+
+#profile("Osazee",20)
+
+#def myself(name,course,age):
+ # print(f"My name is {name}, i am studying {course} and i am {age}.")
+
+# myself("marvelous","Science lab technology",24)
+#def repeat_lyrics():
+#  print_lyrics()
+
+#  repeat_lyrics()
+
+#def get_country(country="USA"):
+#  print(country)
+
+#get_country()
+#def add_two(x,y):
+#  answer=x + y
+ # return answer
+
+
+#def add_two(x,y):
+ # total=x+y
+ # return total
+
+#print(add_two(10,15))
+
+
+#price= int(input("what is your price"))
+#hours=int(input("how many hours did you work"))
+#def work_and_price(price,hours):
+ # total_pay=price * hours
+ # final = f"${total_pay}"
+ # return final
+
+#print(work_and_price(price,hours))
+
+#my_dict={
+
+
+ #"brand":"ford",
+ # "model":"mustang",
+ # "year":1964
+#}
+#my_dict["car"]="lexus"
+#
+# 
+# print my_dict
+
+
+
+
+
+
